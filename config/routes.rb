f3f57f1193b0dde6959b2d3a55d2c6610f461884
sonehanae <64586084+sonehanae@users.noreply.github.com>
2022-01-20 13:36:55 +0900

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # ルーティングの変更は、URL=>”コントローラー＃アクション”
+  post "logout" => "users#logout"
+  post "login" => "users#login"
+  get "login" => "users#login_form"
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create" 
+  get "signup" => "users#new"
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+  get "/" => "home#top"
+  get "about" => "home#about"
+  get 'users/index'
+  get "users/:id" => "users#show"
+
+end
